589a2249b1e42f08396e673ff70cdd8c37e03113
wangjunchao <1109355524@qq.com>
2021-10-26 11:16:36 +0800

    项目初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+1
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  created () {},
+  mounted () {},
+  methods: {}
 }
 </script>
+
+<style scoped>
+</style>
